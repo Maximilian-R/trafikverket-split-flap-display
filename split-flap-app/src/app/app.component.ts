fc1035db284a42b8d420ea2b956da8e1451cf867
Maximilian <max.ragmo@outlook.com>
2021-10-02 16:40:36 +0200

    search in header

--- split-flap-app/src/app/app.component.ts
+++ split-flap-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITrainStation } from './train-announcement/interfaces/train-station.interface';
 
 @Component({
 	selector: 'app-root',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'split-flap-app';
+
+	trainStation: ITrainStation;
 
 	constructor() {}
 }
