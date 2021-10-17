11ca01e4c825d8cf0f6cf6c2453098b53cd7bb5d
Maximilian <max.ragmo@outlook.com>
2021-10-17 15:44:07 +0200

    delete example component

--- split-flap-app/src/app/train-announcement/train-announcement.module.ts
+++ split-flap-app/src/app/train-announcement/train-announcement.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { DepartureScreenComponent } from './departure-screen/departure-screen.component';
-import { TrainAnnouncementExampleComponent } from './example/train-announcement-example.component';
 import { SplitFlapDepartureComponent } from './split-flap-departure/split-flap-departure.component';
 import { SplitFlapModule } from '../split-flap/split-flap.module';
 import { CoreModule } from '../core/core.module';
@@ -10,8 +9,8 @@ import { ClickOutsideModule } from 'ng-click-outside';
 import { TrainStationSearchComponent } from './train-station-search/train-station-search.component';
 
 @NgModule({
-	declarations: [DepartureScreenComponent, TrainAnnouncementExampleComponent, SplitFlapDepartureComponent, TrainStationSearchComponent],
+	declarations: [DepartureScreenComponent, SplitFlapDepartureComponent, TrainStationSearchComponent],
 	imports: [CommonModule, HttpClientModule, SplitFlapModule, CoreModule, ClickOutsideModule],
-	exports: [DepartureScreenComponent, TrainAnnouncementExampleComponent, SplitFlapDepartureComponent, TrainStationSearchComponent],
+	exports: [DepartureScreenComponent, SplitFlapDepartureComponent, TrainStationSearchComponent],
 })
 export class TrainAnnouncementModule {}
